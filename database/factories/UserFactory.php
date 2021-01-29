863ea59ce630c6775d1343e625dd7da57b98cc50
Rida fd <ridafakherlden@gmail.com>
2021-01-29 07:56:10 +0200

    12. Adding Avatar Faces

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ class UserFactory extends Factory
     protected $model = User::class;
 
     /**
-     * Define the model's default state.
+     * Define the model's default state.    
      *
      * @return array
      */
@@ -28,6 +28,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'avatar' => collect([
+                'ahmad.jpg', 'akio.jpg', 'britney.jpg', 'daniel.jpg',
+                'harry.jpeg', 'john.png', 'jordan.jpg', 'june.jpg',
+                'rob.png', 'marcel.jpg', 'tim.jpg'
+            ])->random()
         ];
     }
 }

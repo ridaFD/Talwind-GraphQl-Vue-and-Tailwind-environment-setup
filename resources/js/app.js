326ec31d784952686428300323a06055beb0b4c1
Rida fd <ridafakherlden@gmail.com>
2021-01-29 05:40:37 +0200

    setup graphql tailwind and vue with laravel

---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+import Vue from 'vue';
+import VueRouter from 'vue-router'
+import './bootstrap'
+import PostList from './components/PostList.vue';
+import Post from './components/Post.vue';
+
+window.Vue = Vue;
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        component: PostList
+    },
+    {
+        path: '/post/:id',
+        name: 'Post',
+        component: Post
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+Vue.use(VueApollo)
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'http://blog.test/graphql'
+})
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
+
+const app = new Vue({
+    el: '#app',
+    apolloProvider,
+    router
+});
---
+++ resources/js/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    post page please welcome
+  </div>
+</template>
+
+<script>
+export default {
+name: "Post"
+}
+</script>
+
+<style scoped>
+
+</style>
